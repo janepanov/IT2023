31f237a376d79d10f263f7b0c5b1b0aaa81a3c50
Jane Panov <panovjane@gmail.com>
2023-04-20 14:28:09 +0200

    Solution

---
+++ ITLab2_groupA/Controllers/EventController.cs
@@ -0,0 +1,105 @@
+﻿using ITLab2_groupA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITLab2_groupA.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<EventModel> events = new List<EventModel>
+        {
+            new EventModel(){Id = 1, Name = "Graduation", Location = "Macedonia"},
+            new EventModel(){Id = 2, Name = "Starting work", Location = "Serbia"},
+            new EventModel(){Id = 3, Name = "Marriage", Location = "Greece"}
+        };
+        // GET: Event
+        public ActionResult ShowEvents()
+        {
+            return View(events);
+        }
+
+        
+        public ActionResult AddNewEvent(EventModel newEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                int newId = events.Any() ? events.Max(e => e.Id) + 1 : 1;
+                newEvent.Id = newId;
+                events.Add(newEvent);
+                return RedirectToAction("EventDetails", new { id = newId });
+            }
+
+            else
+            {
+                return View(newEvent);
+            }
+        }
+
+        public ActionResult EventDetails(string name)
+        {
+            var model = events.LastOrDefault();
+            return View(model);
+        }
+
+        public ActionResult EditEvent(int id)
+        {
+            EventModel eventToEdit = events.LastOrDefault(e => e.Id == id);
+
+            if(eventToEdit == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(eventToEdit);
+        }
+
+        [HttpPost]
+        public ActionResult EditEvent(EventModel eventToEdit)
+        {
+            if (ModelState.IsValid)
+            {
+                int index = events.FindIndex(e => e.Id == eventToEdit.Id);
+
+                if(index == -1)
+                {
+                    return HttpNotFound();
+                }
+
+                events[index] = eventToEdit;
+
+                return RedirectToAction("ShowEvents");
+            }
+
+            return View(eventToEdit);
+        }
+
+        public ActionResult DeleteEvent(int id)
+        {
+            EventModel eventToDelete = events.LastOrDefault(e => e.Id == id);
+
+            if(eventToDelete == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(eventToDelete);
+        }
+
+        [HttpPost, ActionName("DeleteEvent")]
+        public ActionResult ConfirmDeletion(int id)
+        {
+            int index = events.FindIndex(e => e.Id == id);
+
+            if(index == -1)
+            {
+                return HttpNotFound();
+            }
+
+            events.RemoveAt(index);
+            return RedirectToAction("ShowEvents");
+        }
+    }
+}
---
+++ ITLab2_groupA/Models/EventModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ITLab2_groupA.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The event name is required!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The event location is required!")]
+        public string Location { get; set; }
+        public static List<EventModel> Events { get; set; } = new List<EventModel>();
+    }
+}
