22be24ed1180cba061ec21737b6aebdb61c4810e
Hristijan Stefov <kikostefov@gmail.com>
2023-06-11 12:11:40 +0200

    Add AbsenceApp

---
+++ SPE_examples/SPE_2017/AbsenceWebApp/AbsenceWebApp/Models/Absence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AbsenceWebApp.Models
+{
+    public class Absence
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int EmployeeId { get; set; }
+        public bool isApproved { get; set; } = false;
+        public Employee employee { get; set; }
+    }
+}
---
+++ SPE_examples/SPE_2017/AbsenceWebApp/AbsenceWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AbsenceWebApp.Startup))]
+namespace AbsenceWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
