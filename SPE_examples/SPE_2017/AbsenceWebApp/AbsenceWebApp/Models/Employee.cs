67b2ebfe9898a29ddcaed4afc9102e2d9fca0b7b
Hristijan Stefov <kikostefov@gmail.com>
2023-06-11 23:45:14 +0200

    Add AbsenceApp

---
+++ SPE_examples/SPE_2017/AbsenceWebApp/AbsenceWebApp/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AbsenceWebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        [Required]
+        [Range(1, 99)]
+        public int Age { get; set; }
+        public virtual List<WorkGroup> workgroups { get; set; }
+        public List<Absence> absences { get; set; }
+        public Employee()
+        {
+            workgroups = new List<WorkGroup>();
+            absences = new List<Absence>();
+        }
+    }
+}
---
+++ SPE_examples/SPE_2017/AbsenceWebApp/AbsenceWebApp/Models/WorkGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AbsenceWebApp.Models
+{
+    public class WorkGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WorkLocation { get; set; }
+        public string Building { get; set; }
+        public virtual List<Employee> employees { get; set; }
+        public WorkGroup()
+        {
+            employees = new List<Employee>();
+        }
+    }
+}
