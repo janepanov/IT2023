db4c92286139aa962a6b997d8318177daf68f1fe
Hristijan Stefov <kikostefov@gmail.com>
2023-06-11 12:15:50 +0200

    Add AutoApp

---
+++ SPE_examples/Exam_Auto/AutoServicWebApp/AutoService/Models/AutoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoService.Models
+{
+    public class AutoService
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public virtual List<Car> cars { get; set; }
+        public AutoService()
+        {
+            cars = new List<Car>();
+        }
+    }
+}
---
+++ SPE_examples/Exam_Auto/AutoServicWebApp/AutoService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoService.Startup))]
+namespace AutoService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
